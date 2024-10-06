d061fc730891076d865d4933c8a10e3433d4525b
przemyslawdrzewicz <>
2024-10-06 14:48:51 +0200

    add badge to shopping cart

--- frontend/components/layout/app-bar/cart.vue
+++ frontend/components/layout/app-bar/cart.vue
@@ -1,7 +1,15 @@
 <template>
   <v-btn variant="text" icon to="/cart" :active="false">
-    <icons-cart />
+    <v-badge v-if="items.length" :content="items.length">
+      <icons-cart />
+    </v-badge>
+    <icons-cart v-else />
   </v-btn>
 </template>
 
-<script setup></script>
+<script setup>
+import { useCartStore } from '@/store/cart'
+const cartStore = useCartStore()
+
+const { items } = toRefs(cartStore)
+</script>
